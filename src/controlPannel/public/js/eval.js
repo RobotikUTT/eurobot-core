911cd02976ad5e5e15631eb45557a082bbfb84b6
Axel Mousset <pymzorr@gmail.com>
2015-04-24 01:21:39 +0200

    Add input history

--- src/controlPannel/public/js/eval.js
+++ src/controlPannel/public/js/eval.js
@@ -1,17 +1,34 @@
 (function () {
   'use strict';
 
-  var $eval = $('#eval');
+  var $eval   = $('#eval');
+  var history = [];
+  var index   = 0;
 
   $eval.keyup(function (e) {
     if (e.keyCode === 13 && (e.altKey || e.metaKey || e.shiftKey || e.ctrlKey)) {
       doSend($eval.val());
       e.stopPropagation();
     }
+    else if (e.keyCode == 38) {
+      if (index) {
+        index--;
+        $eval.val(history[index]);
+      }
+    }
+    else if (e.keyCode == 40) {
+      if (index < history.length) {
+        index++;
+        $eval.val(history[index]);
+      }
+    }
   });
 
   function doSend (code) {
     if (code) {
+      history.push(code);
+      index++;
+
       window.robotik.io.emit('eval', code);
       $eval.val('');
     }
