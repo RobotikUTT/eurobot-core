61893e12cf93700fd0d73b26f75a014624f5a29d
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-04-23 23:48:15 +0200

    Eval code from interface

---
+++ src/controlPannel/public/js/eval.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  var $eval = $('#eval');
+
+  $eval.keyup(function (e) {
+    if (e.keyCode === 13 && (e.altKey || e.metaKey || e.shiftKey || e.ctrlKey)) {
+      doSend($eval.val());
+      e.stopPropagation();
+    }
+  });
+
+  function doSend (code) {
+    window.robotik.io.emit('eval', code);
+    $eval.val('');
+  }
+
+}());
