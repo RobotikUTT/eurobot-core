a4d7c851465e4e2c8fab40b2989a6f84bdbce10a
Axel Mousset <pymzorr@gmail.com>
2015-08-17 12:06:46 +0200

    Add missing packages; Add new front-end packages along with post installation script

--- scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gulp build
-gulp copy
-cd ./build/src/controlPannel/public
-bower install
---
+++ scripts/postInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CSS_LIB_DIST="src/controlPanel/public/css/lib"
+JS_LIB_DIST="src/controlPanel/public/js/lib"
+
+## Copy client-side library into appropriate folder
+
+# CSS libs
+cp node_modules/bootstrap/dist/css/bootstrap.min.css $CSS_LIB_DIST
+cp node_modules/font-awesome/css/font-awesome.min.css $CSS_LIB_DIST
+cp -r node_modules/font-awesome/fonts $CSS_LIB_DIST/../
+
+# JS libs
+cp node_modules/bootstrap/dist/js/bootstrap.min.js $JS_LIB_DIST
+cp node_modules/jquery/dist/jquery.min.js $JS_LIB_DIST
+cp node_modules/flot/jquery.flot.js $JS_LIB_DIST
+
+## Build and copy client-side files into build folder
+gulp
