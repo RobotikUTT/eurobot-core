e567f118f37b23faa746d890cc0a9c2a2d1439e8
Aurélien Labate <aurelien-git@labate.me>
2015-08-17 19:25:34 +0200

    Add the possiblity to only enable a part of controls (only rotation, only clamp or only forward/backward) on the control panel

--- src/controlPannel/public/js/manualcontrols.js
+++ src/controlPannel/public/js/manualcontrols.js
@@ -5,31 +5,69 @@
 	var left = 0;
 	var right = 0;
 	var leftCoef = 1.3;
+	var controlMode = 'init';
 
-	setInterval(function() {
-		if(!key.up && !key.down) {
-			window.robotik.io.emit('stepStop', { motor : 'elev' } );
-			console.log(key.up + ' ' + key.down)
+
+	function setMode(mode)
+	{
+		if(mode === 'normal')
+		{
+			if(controlMode !== 'init')
+			{
+				document.body.innerHTML = '<h1>Normal (Actualisez la page)</h1>'
+											+ '<i style="font-size:2em;">Touches : ZQSD + Flèches</i><br/>'
+											+ '<img src="images/robotik.jpg">';
+				document.body.style.textAlign = 'center';
+				document.body.style.color = 'gray';
+			}
+			controlMode = mode;
+			localStorage.setItem('controlmode', mode);
 		}
-		// if(!key.left && !key.right) {
-		// 	window.robotik.io.emit('stepStop', { motor : 'clamp' } );
-		// }
-		if(!key.z && !key.q && !key.s && !key.d) {
-			window.robotik.io.emit('runMotor', { motor : 'left', pwm : 0 } );
-			window.robotik.io.emit('runMotor', { motor : 'right', pwm : 0 } );
+		else if(mode === 'power')
+		{
+			document.body.innerHTML = '<h1>Puissance</h1>'
+											+ '<i style="font-size:2em;">Touches : Z et Q</i><br/>'
+											+ '<img src="images/robotik.jpg">';
+			document.body.style.textAlign = 'center';
+			document.body.style.color = 'gray';
+			controlMode = mode;
+			localStorage.setItem('controlmode', mode);
 		}
-	}, 500);
+		else if(mode === 'rotation')
+		{
+			document.body.innerHTML = '<h1>Rotation</h1>'
+											+ '<i style="font-size:2em;">Touches : Q et D</i><br/>'
+											+ '<img src="images/robotik.jpg">';
+			document.body.style.textAlign = 'center';
+			document.body.style.color = 'gray';
+			controlMode = mode;
+			localStorage.setItem('controlmode', mode);
+		}
+		else if(mode === 'clamp')
+		{
+			document.body.innerHTML = '<h1>Pince</h1>'
+											+ '<i style="font-size:2em;">Touches :</i><br/>'
+											+ '<i style="font-size:2em;">↑↓ pour monter et déscendre</i><br/>'
+											+ '<i style="font-size:2em;">←→ pour fermer et ouvrir</i><br/>'
+											+ '<img src="images/robotik.jpg">';
+			document.body.style.textAlign = 'center';
+			document.body.style.color = 'gray';
+			controlMode = mode;
+			localStorage.setItem('controlmode', mode);
+		}
+	}
+	setMode(localStorage.getItem("controlmode"));
 
 	$(document).keydown(function(event) {
 		//filter textarea and input
 		var emit = false;
 		var tag = event.target.tagName.toLowerCase();
 		if(tag != 'input' && tag != 'textarea') {
-			// console.log("Key: " + event.which);
+			console.log("Key: " + event.which);
 			switch(event.which)
 			{
 				case 90: // Z
-					if(key.z !== true)
+					if(key.z !== true && (controlMode === 'normal' || controlMode === 'power'))
 					{
 						emit = true;
 						key.z = true;
@@ -40,17 +78,17 @@
 					break;
 
 				case 81: // Q
-					if(key.q !== true)
+					if(key.q !== true && (controlMode === 'normal' || controlMode === 'rotation'))
 					{
 						emit = true;
 						key.q = true;
 						right += 80;
-						left -= Math.floor(40*leftCoef);
+						left -= Math.floor(60*leftCoef);
 					}
 					break;
 
 				case 83: // S
-					if(key.s !== true)
+					if(key.s !== true && (controlMode === 'normal' || controlMode === 'power'))
 					{
 						emit = true;
 						key.s = true;
@@ -60,17 +98,17 @@
 					break;
 
 				case 68: // D
-					if(key.d !== true)
+					if(key.d !== true && (controlMode === 'normal' || controlMode === 'rotation'))
 					{
 						emit = true;
 						key.d = true;
 						left += Math.floor(80*leftCoef);
-						right -= 40;
+						right -= 60;
 					}
 					break;
 
 				case 32: // Space
-					if(key.space !== true)
+					if(key.space !== true && controlMode === 'normal')
 					{
 						emit = true;
 						key.space = true;
@@ -87,28 +125,28 @@
 					break;
 
 				case 38: // Up
-					if(key.up !== true)
+					if(key.up !== true && (controlMode === 'normal' || controlMode === 'clamp'))
 					{
 						key.up = true;
 						window.robotik.io.emit('stepGoto', { motor : 'elev', pos : 10000000 } );
 					}
 					break;
 				case 40: // Down
-					if(key.down !== true)
+					if(key.down !== true && (controlMode === 'normal' || controlMode === 'clamp'))
 					{
 						key.down = true;
 						window.robotik.io.emit('stepGoto', { motor : 'elev', pos : -10000000 } );
 					}
 					break;
 				case 37: // Left
-					if(key.left !== true)
+					if(key.left !== true && (controlMode === 'normal' || controlMode === 'clamp'))
 					{
 						key.left = true;
 						window.robotik.io.emit('stepGoto', { motor : 'clamp', pos : 10000000 } );
 					}
 					break;
 				case 39: // Right
-					if(key.right !== true)
+					if(key.right !== true && (controlMode === 'normal' || controlMode === 'clamp'))
 					{
 						key.right = true;
 						window.robotik.io.emit('stepGoto', { motor : 'clamp', pos : -10000000 } );
@@ -128,15 +166,61 @@
 				// 		window.robotik.io.emit('stepInit', { motor : 'clamp' } );
 				// 	}
 				// 	break;
+				//
+
+				case 88: //x
+					key.x = true;
+					break;
+				case 176: //²
+					key.square = true;
+					break;
+				case 61: //=
+					key.eq = true;
+					break;
+				case 85: //u
+					key.u = true;
+					break;
+				case 73: //i
+					key.i = true;
+					break;
+				case 79: //o
+					key.o = true;
+					break;
+				case 80: //p
+					key.p = true;
+					break;
 			}
 
-			if(emit == true)
+			if(emit === true)
 			{
 				window.robotik.io.emit('runMotor', { motor : 'left', pwm : left } );
 				window.robotik.io.emit('runMotor', { motor : 'right', pwm : right } );
 				emit = false;
 			}
-		}
+
+			// Manual control only mode
+			if(key.x === true && key.square === true && key.eq === true)
+			{
+				//Remove everithing on the screen and put a Robotik Logo
+   				if(key.u === true)
+   				{
+   					setMode('normal');
+   				}
+   				else if(key.i === true)
+   				{
+   					setMode('power');
+   				}
+   				else if(key.o === true)
+   				{
+   					setMode('rotation');
+   				}
+   				else if(key.p === true)
+   				{
+   					setMode('clamp');
+   				}
+
+   			}
+   		}
 	});
 
 
@@ -235,9 +319,32 @@
 				// 		key.ugrave = false;
 				// 	}
 				// 	break;
+				//
+
+				case 88: //x
+					key.x = false;
+					break;
+				case 176: //²
+					key.square = false;
+					break;
+				case 61: //=
+					key.eq = false;
+					break;
+				case 85: //u
+					key.u = false;
+					break;
+				case 73: //i
+					key.i = false;
+					break;
+				case 79: //o
+					key.o = false;
+					break;
+				case 80: //p
+					key.p = false;
+					break;
 			}
 
-			if(emit == true)
+			if(emit === true)
 			{
 				window.robotik.io.emit('runMotor', { motor : 'left', pwm : left } );
 				window.robotik.io.emit('runMotor', { motor : 'right', pwm : right } );
