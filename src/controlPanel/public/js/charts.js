045415f46bc3218c889af67d5822ea369c398288
Axel Mousset <pymzorr@gmail.com>
2015-08-22 15:29:54 +0200

    Charts are now responsive

--- src/controlPanel/public/js/charts.js
+++ src/controlPanel/public/js/charts.js
@@ -3,7 +3,6 @@ var MotionChart = function($container) {
     this.series = [{ data: [] }, { data: [] }];
     this.data = { position: [], velocity: [] };
     this.$plot = null;
-    this.initialized = false;
     this.refreshPeriod = 100; //ms
 
     var that = this;
@@ -14,7 +13,7 @@ var MotionChart = function($container) {
 
         this.series[0].data = [];
         this.series[1].data = [];
-        for (var i = 0; i < this.maximum; ++i)
+        for (var i = 0; i < this.data.position.length; ++i)
         {
             this.series[0].data.push([i, this.data.position[i]]);
             this.series[1].data.push([i, this.data.velocity[i]]);
@@ -30,65 +29,57 @@ var MotionChart = function($container) {
         // However, 3000 point min
         // this.maximum = (this.maximum > 3000) ? this.maximum : 3000;
 
-        if (!this.initialized) {
-            // Init data
-            for (var i = 0; i < this.maximum; i++)
-            {
-                this.data.position[i] = 0;
-                this.data.velocity[i] = 0;
-            }
-            this.initialized = true;
-        }
-
         this.$plot =  $.plot(this.$container, this.series, {
-        grid: {
-            borderWidth: 1,
-            minBorderMargin: 20,
-            labelMargin: 10,
-            backgroundColor: {
-                colors: ["#fff", "#e4f4f4"]
-            },
-            margin: {
-                top: 8,
-                bottom: 20,
-                left: 20
-            },
-            markings: function(axes) {
-                var markings = [];
-                var xaxis = axes.xaxis;
-                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
-                    markings.push({ xaxis: { from: x, to: x + xaxis.tickSize }, color: "rgba(232, 232, 255, 0.2)" });
+            grid: {
+                borderWidth: 1,
+                minBorderMargin: 20,
+                labelMargin: 10,
+                backgroundColor: {
+                    colors: ["#fff", "#e4f4f4"]
+                },
+                margin: {
+                    top: 8,
+                    bottom: 20,
+                    left: 20
+                },
+                markings: function(axes) {
+                    var markings = [];
+                    var xaxis = axes.xaxis;
+                    for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                        markings.push({ xaxis: { from: x, to: x + xaxis.tickSize }, color: "rgba(232, 232, 255, 0.2)" });
+                    }
+                    return markings;
                 }
-                return markings;
+            },
+            xaxis: {
+                min: 0,
+                max: that.maximum,
+                tickFormatter: function() {
+                    return "";
+                }
+            },
+            yaxis: {
+                min: 0,
+                max: 1
+            },
+            legend: {
+                show: true
             }
-        },
-        xaxis: {
-            min: 0,
-            max: that.maximum,
-            tickFormatter: function() {
-                return "";
-            }
-        },
-        yaxis: {
-            min: 0,
-            max: 1
-        },
-        legend: {
-            show: true
-        }
-    });
-        this.render();
+        });
 
-    }
+        this.render();
+    };
 
     this.addPoints = function(newData) {
         if (!this.$plot)
             return
 
-        this.data.position = this.data.position.slice(1);
+        if (this.data.length >= this.maximum) {
+            this.data.position = this.data.position.slice(1);
+            this.data.velocity = this.data.velocity.slice(1);
+        }
         this.data.position.push(newData.position);
         this.data.velocity.push(newData.velocity);
-        this.data.velocity = this.data.velocity.slice(1);
     }
 
     this.resize = function() {
@@ -110,7 +101,6 @@ $(function() {
     charts.orientationChart = new MotionChart($('#orientationChart'));
 
     $(window).on('resize', function() {
-        console.log('ici');
         charts.distanceChart.resize();
         charts.orientationChart.resize();
     });
